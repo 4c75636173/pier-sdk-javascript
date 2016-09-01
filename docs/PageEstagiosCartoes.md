bdc8f917c054d3f77958df26234b5e02cee08169
Filipe Germano <filipe.germano@conductor.com.br>
2016-09-01 08:09:18 -0300

    Ajustando Tipo de retorno de Lista para Page.

--- docs/PageEstagiosCartoes.md
+++ docs/PageEstagiosCartoes.md
@@ -3,6 +3,19 @@
 ## Properties
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**estagioCartoes** | [**PageApiOfEstagioCartao**](PageApiOfEstagioCartao.md) |  | [optional] 
+**content** | [**[EstagioCartao]**](EstagioCartao.md) |  | [optional] 
+**first** | **Boolean** |  | [optional] 
+**firstPage** | **Boolean** |  | [optional] 
+**hasContent** | **Boolean** |  | [optional] 
+**hasNextPage** | **Boolean** |  | [optional] 
+**hasPreviousPage** | **Boolean** |  | [optional] 
+**last** | **Boolean** |  | [optional] 
+**nextPage** | **Integer** |  | [optional] 
+**_number** | **Integer** |  | [optional] 
+**numberOfElements** | **Integer** |  | [optional] 
+**previousPage** | **Integer** |  | [optional] 
+**size** | **Integer** |  | [optional] 
+**totalElements** | **Integer** |  | [optional] 
+**totalPages** | **Integer** |  | [optional] 
 
 
--- src/model/PageEstagiosCartoes.js
+++ src/model/PageEstagiosCartoes.js
@@ -1,18 +1,18 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['../ApiClient', './PageApiOfEstagioCartao'], factory);
+    define(['../ApiClient', './EstagioCartao'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('./PageApiOfEstagioCartao'));
+    module.exports = factory(require('../ApiClient'), require('./EstagioCartao'));
   } else {
     // Browser globals (root is window)
     if (!root.Pier) {
       root.Pier = {};
     }
-    root.Pier.PageEstagiosCartoes = factory(root.Pier.ApiClient, root.Pier.PageApiOfEstagioCartao);
+    root.Pier.PageEstagiosCartoes = factory(root.Pier.ApiClient, root.Pier.EstagioCartao);
   }
-}(this, function(ApiClient, PageApiOfEstagioCartao) {
+}(this, function(ApiClient, EstagioCartao) {
   'use strict';
 
   /**
@@ -30,6 +30,19 @@
   var exports = function() {
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
   };
 
   /**
@@ -43,8 +56,47 @@
     if (data) { 
       obj = obj || new exports();
 
-      if (data.hasOwnProperty('estagioCartoes')) {
-        obj['estagioCartoes'] = PageApiOfEstagioCartao.constructFromObject(data['estagioCartoes']);
+      if (data.hasOwnProperty('content')) {
+        obj['content'] = ApiClient.convertToType(data['content'], [EstagioCartao]);
+      }
+      if (data.hasOwnProperty('first')) {
+        obj['first'] = ApiClient.convertToType(data['first'], 'Boolean');
+      }
+      if (data.hasOwnProperty('firstPage')) {
+        obj['firstPage'] = ApiClient.convertToType(data['firstPage'], 'Boolean');
+      }
+      if (data.hasOwnProperty('hasContent')) {
+        obj['hasContent'] = ApiClient.convertToType(data['hasContent'], 'Boolean');
+      }
+      if (data.hasOwnProperty('hasNextPage')) {
+        obj['hasNextPage'] = ApiClient.convertToType(data['hasNextPage'], 'Boolean');
+      }
+      if (data.hasOwnProperty('hasPreviousPage')) {
+        obj['hasPreviousPage'] = ApiClient.convertToType(data['hasPreviousPage'], 'Boolean');
+      }
+      if (data.hasOwnProperty('last')) {
+        obj['last'] = ApiClient.convertToType(data['last'], 'Boolean');
+      }
+      if (data.hasOwnProperty('nextPage')) {
+        obj['nextPage'] = ApiClient.convertToType(data['nextPage'], 'Integer');
+      }
+      if (data.hasOwnProperty('number')) {
+        obj['number'] = ApiClient.convertToType(data['number'], 'Integer');
+      }
+      if (data.hasOwnProperty('numberOfElements')) {
+        obj['numberOfElements'] = ApiClient.convertToType(data['numberOfElements'], 'Integer');
+      }
+      if (data.hasOwnProperty('previousPage')) {
+        obj['previousPage'] = ApiClient.convertToType(data['previousPage'], 'Integer');
+      }
+      if (data.hasOwnProperty('size')) {
+        obj['size'] = ApiClient.convertToType(data['size'], 'Integer');
+      }
+      if (data.hasOwnProperty('totalElements')) {
+        obj['totalElements'] = ApiClient.convertToType(data['totalElements'], 'Integer');
+      }
+      if (data.hasOwnProperty('totalPages')) {
+        obj['totalPages'] = ApiClient.convertToType(data['totalPages'], 'Integer');
       }
     }
     return obj;
@@ -52,9 +104,74 @@
 
 
   /**
-   * @member {module:model/PageApiOfEstagioCartao} estagioCartoes
+   * @member {Array.<module:model/EstagioCartao>} content
    */
-  exports.prototype['estagioCartoes'] = undefined;
+  exports.prototype['content'] = undefined;
+
+  /**
+   * @member {Boolean} first
+   */
+  exports.prototype['first'] = undefined;
+
+  /**
+   * @member {Boolean} firstPage
+   */
+  exports.prototype['firstPage'] = undefined;
+
+  /**
+   * @member {Boolean} hasContent
+   */
+  exports.prototype['hasContent'] = undefined;
+
+  /**
+   * @member {Boolean} hasNextPage
+   */
+  exports.prototype['hasNextPage'] = undefined;
+
+  /**
+   * @member {Boolean} hasPreviousPage
+   */
+  exports.prototype['hasPreviousPage'] = undefined;
+
+  /**
+   * @member {Boolean} last
+   */
+  exports.prototype['last'] = undefined;
+
+  /**
+   * @member {Integer} nextPage
+   */
+  exports.prototype['nextPage'] = undefined;
+
+  /**
+   * @member {Integer} number
+   */
+  exports.prototype['number'] = undefined;
+
+  /**
+   * @member {Integer} numberOfElements
+   */
+  exports.prototype['numberOfElements'] = undefined;
+
+  /**
+   * @member {Integer} previousPage
+   */
+  exports.prototype['previousPage'] = undefined;
+
+  /**
+   * @member {Integer} size
+   */
+  exports.prototype['size'] = undefined;
+
+  /**
+   * @member {Integer} totalElements
+   */
+  exports.prototype['totalElements'] = undefined;
+
+  /**
+   * @member {Integer} totalPages
+   */
+  exports.prototype['totalPages'] = undefined;
 
 
 
